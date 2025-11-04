1b41267d1ad1b50fb2f3259a2c4daae1292a300f
Frederick2-2 <RunFrederick2@gmail.com>
2025-11-04 11:41:10 -0800

    player jump and gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
 
     public float speed = 10f;
+    public float jump = 7f;
+    public float fall = 7f;
+    public int fallAmount = 1;
+    public float floorCheckDist = 1.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,22 +31,64 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        PlayerJump();
     }
 
     
-    
+    /// <summary>
+    /// This will let the player move left and right in the levels
+    /// </summary>
     private void PlayerMovement()
     {
         if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             direction = Vector3.left;
             rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
+
+
+            //Rotates the object from its current rotation value by the new amount
+            //Quaternion rotationalAngle = Quaternion.Euler(0f, 180f, 0f);
+
+            //sets the rotation value to a fixed value
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+
         }
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             direction = Vector3.right;
             rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
         }
+    }
+    /// <summary>
+    /// this will allow the player to jump up and if they want go down quicker
+    /// </summary>
+    private void PlayerJump()
+    {
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && IsGrounded())
+        {
+            //AddForce is for jumping and using earth gravity, can ajust gravity... Impulse give it the tappy state
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        }
+        else if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && fallAmount > 0)
+        {
+            //This is to force the player down if they are in air and want to go down but cant spam infinetly
+            rb.AddForce(Vector3.down * fall, ForceMode.Impulse);
+
+            fallAmount--;
+        }
+    }
+    private bool IsGrounded()
+    {
+        bool isGrounded = false;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, floorCheckDist))
+        {
+            isGrounded = true;
+            fallAmount = 1;
+        }
+        return isGrounded;
     }
 
 }
