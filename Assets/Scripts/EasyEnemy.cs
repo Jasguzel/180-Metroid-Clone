37b76a55a8a9807c4c42f73b15b6bd9a75d3d10b
Frederick2-2 <RunFrederick2@gmail.com>
2025-11-06 11:56:09 -0800

    easyenemy movement

--- Assets/Scripts/EasyEnemy.cs
+++ Assets/Scripts/EasyEnemy.cs
@@ -10,15 +10,39 @@ using UnityEngine;
  */
 public class EasyEnemy : MonoBehaviour
 {
+    public Transform leftPoint;
+    public Transform rightPoint;
+    public float speed = 10;
+
+    private Vector3 direction;
+    private Vector3 startLeftPos;
+    private Vector3 startRightPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = Vector3.left;
+        startLeftPos = leftPoint.position;
+        startRightPos = rightPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+    private void Move()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        //this will ask if the player is going left and is at the left position boundary
+        if (direction == Vector3.left && transform.position.x <= startLeftPos.x)
+        {
+            direction = Vector3.right;
+        }
+        //this will ask if the player is going right and is at the right position boundary
+        else if (direction == Vector3.right && transform.position.x >= startRightPos.x)
+        {
+            direction = Vector3.left;
+        }
+    }//MUST MAKE SURE THE LEFT AND RIGHT HAVE DIFFERENT GREATER/LESS THAN SYMBOLS
+    
 }
